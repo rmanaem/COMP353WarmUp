bd07d73555d7093374a4d1c3f3e05791a52739b7
Dwarf1er <poulin.antoine@hotmail.com>
2021-02-11 12:38:27 -0500

    added query buttons to search page

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.layout')
 
 @section('content')
-    <p>SEARCH</p>
+    <div class="content">
+        <div class="title m-b-md">
+            Database Search Tools
+        </div>
+        <div class="">
+            <button class="button">Group Zone - Montreal Primary Grade 1, Group 1</button>
+            <button class="button">Positives - January 10th, 2021</button>
+            <button class="button">Diagnoses - 95 Robert Street</button>
+            <button class="button">People Living With - Roger Macdonald</button>
+            <button class="button">Group Zones - Roger Macdonald</button>
+            <button class="button">Public Health Workers - Viau Public Health Center</button>
+        </div>
+    </div>
 @endsection
